7d28e44e6ac679a66a63cbdb6c9a5c67de296955
Ashley OGorman <ashley87105@gmail.com>
2024-05-06 14:22:29 -0500

    debug checkout

--- src/main/java/edu/mu/TransactionSingleton.java
+++ src/main/java/edu/mu/TransactionSingleton.java
@@ -94,11 +94,12 @@ public class TransactionSingleton {
      * Checkout checks out the user and uses strategy method to checkout with cash or card
      * 
      */
-    public boolean checkout() {
+    public void checkout() {
     	int valid = 0;
     	Scanner scanner = new Scanner(System.in);
     	ICheckoutStrategy checkoutStrategy = null;
 
+    	
 		if(bookBoy.BookClub(scanner)){
 			this.total = this.total * 0.8;
 		} else {
@@ -125,13 +126,10 @@ public class TransactionSingleton {
         		System.out.println("Invalid input. Please enter a number.");
         	}
     	}
+    	checkoutStrategy.checkout(total);
+    	System.out.println("Thank you for shopping with us!");
     	scanner.close();
-    	if(checkoutStrategy.checkout(total)) {
-    		System.out.println("Thank you for shopping with us!");
-    		clearCart();
-    		return true;
-    	}
-    	return false;
+    	clearCart();
     }
     
 }
