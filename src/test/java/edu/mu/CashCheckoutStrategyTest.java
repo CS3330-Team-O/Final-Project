fc8b7975b657cf95935b7e6cbdea657327eae61f
davidAllenStephan <david.marino03@hotmail.com>
2024-05-06 16:53:26 -0500

    CashCheckout test

---
+++ src/test/java/edu/mu/CashCheckoutStrategyTest.java
@@ -0,0 +1,43 @@
+package edu.mu;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Order;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+
+import java.io.ByteArrayInputStream;
+import java.io.InputStream;
+import java.util.Scanner;
+
+public class CashCheckoutStrategyTest{
+	
+	private InputStream stdin;
+	
+	@BeforeEach
+	void setUp() throws Exception {
+		System.out.println("Running Before Each");
+        stdin = System.in; // Backup of System.in
+	}
+
+	@AfterEach
+	void tearDown() throws Exception {
+        System.setIn(stdin); // Restore original System.in
+	}
+
+	@Test
+	void checkout() {
+		CashCheckoutStrategy ccs = new CashCheckoutStrategy();
+		String input = "\n10.0\n90.0";
+        InputStream inputStream = new ByteArrayInputStream(input.getBytes());
+        System.setIn(inputStream);
+        ccs.checkout(100.0);
+	}
+
+    
+
+}
