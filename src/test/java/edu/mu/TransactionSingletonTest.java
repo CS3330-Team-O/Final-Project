9370f24c1e82701715cdecfc2eca4b2a9e6e8f67
davidAllenStephan <david.marino03@hotmail.com>
2024-05-06 16:49:38 -0500

    transaction test

--- src/test/java/edu/mu/TransactionSingletonTest.java
+++ src/test/java/edu/mu/TransactionSingletonTest.java
@@ -6,13 +6,19 @@ import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
+
+import java.io.ByteArrayInputStream;
+import java.io.InputStream;
+
 
 class TransactionSingletonTest {
 	
 	private static TransactionSingleton transactionSingleton;
 	private static InventoryManager inventoryManager;
 	private static Product p = null;
+	private InputStream stdin;
 
 	@BeforeAll
 	static void setUpBeforeClass() throws Exception {
@@ -29,10 +35,12 @@ class TransactionSingletonTest {
 	@BeforeEach
 	void setUp() throws Exception {
 		System.out.println("Running Before Each");
+        stdin = System.in; // Backup of System.in
 	}
 
 	@AfterEach
 	void tearDown() throws Exception {
+        System.setIn(stdin); // Restore original System.in
 	}
 	
 	@Test
@@ -50,5 +58,22 @@ class TransactionSingletonTest {
 	void testremoveItemFromCart() {
 		assertEquals(true, transactionSingleton.removeItemFromCart(p));
 	}
+	
+	@Test
+	void checkout() {
+		String input = "1\nmarino, david";
+        InputStream inputStream = new ByteArrayInputStream(input.getBytes());
+        System.setIn(inputStream);
+        transactionSingleton.checkout();   
+        
+        input = "1\nmarino, david\n2";
+        inputStream = new ByteArrayInputStream(input.getBytes());
+        System.setIn(inputStream);
+        transactionSingleton.checkout();  
+        
+        assertTrue(true);
+	}
+	
+	
 
 }
